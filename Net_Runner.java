f2d177f8e24cf27f271df5cd3f3d09b590a9e69c
jaredc23 <38102180+jaredc23@users.noreply.github.com>
2019-02-24 22:50:45 -0500

    Update Net_Runner.java

--- Net_Runner.java
+++ Net_Runner.java
@@ -1,16 +1,22 @@
 
+
 public class Net_Runner
 {
 	public static void main (String[] args)
 	{
 		int b[] = {3,2,3};
 		NeuralNet net = new NeuralNet(b);
-		double c[] = {.45, .23, .55};
+		double c[] = {.45, .22, .11};
 		System.out.println (net.forwardPropogate(c));
-		net.printWeights();
-		System.out.println ("\n************************");
-		net.printBiases();
-		System.out.println ("\n************************");
+		net.printNeurons();
+		
+		double d[] = {1, 0.0, 0.0};
+		for(int i = 0; i < 1000; i++)
+		{
+			net.teach(c,d);
+		}
+		
+		System.out.println ("\n***Old Neurons above . new Neurons below*******");
 		net.printNeurons();
 	}
 }
