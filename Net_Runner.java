c614c87e8105204c1e3f7a31c63678d57dc8f944
jaredc23 <38102180+jaredc23@users.noreply.github.com>
2019-10-22 23:05:41 -0400

    Update Net_Runner.java

--- Net_Runner.java
+++ Net_Runner.java
@@ -3,18 +3,33 @@ public class Net_Runner
 {
 	public static void main (String[] args)
 	{
-		int b[] = {3,2,3};
+		int b[] = {3,2,3}; //This is the setup for the net: the physica net looks like the picture at the bottom
 		NeuralNet net = new NeuralNet(b, true);
-		double c[] = {.45, .22, .11};
-		net.forwardPropogate(c);
+		double c[] = {.45, .22, .11}; //Random input values.
+		net.forwardPropogate(c); //This is to fill all the neurons with values, so the initial state can be printed
 		net.printNeurons();
-		double d[] = {1, 0.0, 0.0};
-		for(int i = 0; i < 100; i++)
+		double d[] = {1, 0.0, 0.0}; //Expected outputs
+		for(int i = 0; i < 100; i++) //This is to teach 100 times
 		{
-			net.teach(c,d);
+			net.teach(c,d); //Teaches the AI with the inputs C, and expected output D. Each time this is called the output becomes closer to expected outputs
 		}
 		
 		System.out.println ("\n***Old Neurons above. new Neurons below*******");
 		net.printNeurons();
 	}
 }
+
+/*
+
+    ( )           ( )
+           ( )     
+    ( )           ( )
+           ( )
+    ( )           ( )
+
+A setup of {3,2,3} would result in a net that looks like this.
+each neuron in each column of "( )" connected to each neuron in the next
+
+
+
+*/
